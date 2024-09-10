16e196de9e8cfce74ec7172e6543bde123ea77e2
Maelan <maelan.trauchessec@etu.umontpellier.fr>
2024-09-10 16:43:39 +0200

    "inserer"

--- src/main/java/fr/iut/editeur/commande/CommandeInserer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeInserer.java
@@ -2,16 +2,15 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
-public class CommandeClear extends CommandeDocument {
-
-    public CommandeClear(Document document, String[] parameters) {
+public class CommandeInserer extends CommandeDocument {
+    public CommandeInserer(Document document, String[] parameters) {
         super(document, parameters);
     }
 
     @Override
     public void executer() {
         // Appel de la méthode clear dans Document
-        this.document.clear();
+        this.document.inserer();
 
         // Exécution de la commande
         super.executer();
